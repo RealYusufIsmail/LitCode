e8ff9afa5525f4c64f59f523a539dcaa47d6ea8c
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-10-01 10:11:46 +0100

    Done question 217

---
+++ src/main/kotlin/217/Solution.kt
@@ -0,0 +1,20 @@
+package `217`
+
+class Solution {
+
+    /**
+     * We do a for loop where we from the start till the end. With this for loop we check if the set which has been created contains a duplicate number.
+     * If it does not it adds that number to the set and rebates until the end of the array. If there is no duplicate it will return false.
+     */
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val set = mutableSetOf<Int>()
+        for (i in nums) {
+            if (set.contains(i)) {
+                return true
+            }
+            set.add(i)
+        }
+
+        return false
+    }
+}
